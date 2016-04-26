07f01e2ad626c12192351883478bb1d204a483e0
Thomas Bruderer <apophis@apophis.ch>
2016-04-26 15:10:16 +0200

    added blank detection to thumbnails

--- lib/thumbnail.rb
+++ lib/thumbnail.rb
@@ -13,13 +13,21 @@ module VCSRuby
     def initialize capper, video
       @capper = capper
       @video = video
+      @blank_threshold = 0.10
     end
 
     def capture 
       @capper.grab @time, @image_path
-      image = MiniMagick::Image.open(@image_path)
+      image = MiniMagick::Image.open @image_path
       image.resize "#{width}x#{height}!"
       image.write @image_path
+    end
+
+    def blank?
+      image = MiniMagick::Image.open @image_path
+      image.colorspace 'Gray'
+      mean = image['%[fx:image.mean]'].to_f
+      return mean < @blank_threshold
     end
   end
 end
